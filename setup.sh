f500cab9184a8110ab8af750857c2b4be6dfa1b7
tsutsu <tsutsu@debian.local.com>
2016-08-20 04:01:55 +0900

    不要なコピーを削除。

--- setup.sh
+++ setup.sh
@@ -15,5 +15,4 @@ done
 
 if [ ! -e $HOME/.oh-my-zsh ]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-  cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 fi
