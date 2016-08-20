5482328f7d00dffb6431c7981a4e4dc1c16d2fe2
tsutsu <tsutsu@debian.local.com>
2016-08-21 08:43:21 +0900

    rtagsを追加。

---
+++ .emacs.d/config/packages/local.el
@@ -0,0 +1,13 @@
+;;; gtags
+;; http://qiita.com/kota65535/items/39aa4d6e8adf6ab5f98c
+;; http://qiita.com/alpha22jp/items/90f7f2ad4f8b1fa089f4
+;; https://github.com/Andersbakken/rtags
+(package-ensure-install 'rtags)
+(when (require 'rtags nil 'noerror)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (rtags-is-indexed)
+                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
+                (local-set-key (kbd "M-;") 'rtags-find-symbol)
+                (local-set-key (kbd "M-@") 'rtags-find-references)
+                (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
