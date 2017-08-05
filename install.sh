587ecf90952b50bd5869ba8a9526b2e6aa25dca8
tsutsu <tsutsu@example.com>
2017-08-05 09:39:02 +0900

    debian9に向けて修正。

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ DOTFILES_DIR=`pwd`
 cd $HOME
 
 # build-essential
-#sudo aptitude install build-essential -y
+#sudo apt install build-essential -y
 
 # open-vm-tools
-#sudo aptitude install open-vm-tools open-vm-tools-desktop -y
+#sudo apt install open-vm-tools open-vm-tools-desktop -y
 
 # git subversion（git本体は事前にインストール）
 git config --global user.name 'tsutsu'
@@ -21,29 +21,29 @@ git config --global core.precomposeunicode true
 git config --global core.quotepath false
 git config --global alias.mylog 'log --graph --name-status --pretty=format:"%C(red)%h %C(green)%an %Creset%s %C(yellow)%d%Creset"'
 git config --global merge.tool 'meld'
-sudo aptitude install subversion subversion-tools -y
+sudo apt install subversion subversion-tools -y
 
 # terminator zsh tmux
-sudo aptitude install terminator zsh tmux xsel -y
+sudo apt install terminator zsh tmux xsel -y
 
 # python
-sudo aptitude install libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev -y
+sudo apt install libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev -y
 git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
-pyenv install 3.6.1
+pyenv install 3.6.2
 pyenv install 2.7.13
-pyenv global 3.6.1
+pyenv global 3.6.2
 
 # emacs vim
-sudo aptitude install emacs vim -y
+sudo apt install emacs vim -y
 
 # atom（本体は事前にdebファイルからインストール）
 #apm install --packages-file $DOTFILES_DIR/apm_packages.txt
 
 # rtags build
-sudo aptitude install clang libclang-dev cmake -y
+sudo apt install clang libclang-dev cmake -y
 git clone https://github.com/Andersbakken/rtags $HOME/rtags
 cd $HOME/rtags
 git submodule init
@@ -60,14 +60,17 @@ cmake ..
 make -j2 && sudo make install
 
 # crror
-sudo aptitude install golang -y
+sudo apt install golang -y
 git clone https://github.com/bearmini/crror.git ~/crror
 
 # Qt
-sudo aptitude install qt5-default -y
+sudo apt install qt5-default -y
 
 # etc.
-sudo aptitude install ruby nodejs tig libncurses5-dev gdb valgrind strace meld clipit fonts-inconsolata libappindicator-dev curl \
-graphviz doxygen gtk2-engines-murrine gconf2 locate -y
+sudo apt install ruby tig libncurses5-dev gdb valgrind strace meld clipit fonts-inconsolata libappindicator-dev curl \
+graphviz doxygen gconf2 locate -y
 
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+# node(npm)
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo apt install nodejs
+
