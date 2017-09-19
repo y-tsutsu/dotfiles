f6b062770961984f15c1c312aa14c70d30478454
y-tsutsu <tsutsu0928@gmail.com>
2017-09-20 00:42:52 +0900

    install.shを更新。

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ sudo apt install subversion subversion-tools -y
 sudo apt install terminator zsh tmux xsel -y
 
 # python
-sudo apt install libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev -y
+sudo apt install libsqlite3-dev libreadline-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev -y
 git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
