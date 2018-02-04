5199cb02286d4f3651db4162d6aaa506060a8827
tsutsu <tsutsu0928@gmail.com>
2018-02-04 20:18:51 +0900

    sl(pyonpyon)のセットアップ（Linux）

---
+++ pyonpyon/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+WORK=$(cd $(dirname $0) && pwd)
+cd $WORK
+git clone https://github.com/sgymtic/sl.git
+cd ./sl
+make -j2
+git checkout pyonpyon
+make -j2
+sudo ln -s $WORK/sl/pyonpyon /usr/games/sl
