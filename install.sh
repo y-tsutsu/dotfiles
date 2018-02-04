cad9039dc308e7f5c952669b2d374c27f7c5babe
tsutsu <tsutsu0928@gmail.com>
2018-02-04 19:15:24 +0900

    vscode関連のファイルのディレクトリにまとめ．

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install -y code
-$DOTFILES_DIR/install_vscode_extensions.sh
+$DOTFILES_DIR/vscode/install_vscode_extensions.sh
 
 # GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
