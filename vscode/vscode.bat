c1652e3e11513c4052674f7faf54c77a191b0a3f
tsutsu <yusuke.tsutsumi@itage.co.jp>
2018-11-21 10:31:45 +0900

    PathをWindows10の内容に合わせて修正．

--- vscode/vscode.bat
+++ vscode/vscode.bat
@@ -1,5 +1,5 @@
 @echo off
 SET CMDER_ROOT=C:\cmder
-SET PATH=C:\Program Files (x86)\Git\usr\bin;%ConEmuBaseDir%\Scripts;%PATH%
+SET PATH=C:\Program Files\Git\usr\bin;%ConEmuBaseDir%\Scripts;%PATH%
 SET LANG=ja_JP.UTF-8
 "%CMDER_ROOT%\vendor\init.bat"
