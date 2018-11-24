5b7cdff3aa493ed10427d828bea4faab7109d8c2
tsutsu <tsutsu0928@gmail.com>
2018-11-24 23:52:56 +0900

    cmderの環境変数の設定を変更．

--- vscode/vscode.bat
+++ vscode/vscode.bat
@@ -1,5 +1,5 @@
 @echo off
 SET CMDER_ROOT=C:\cmder
-SET PATH=C:\Program Files\Git\usr\bin;%ConEmuBaseDir%\Scripts;%PATH%
+SET PATH=%ConEmuBaseDir%\Scripts;%PATH%
 SET LANG=ja_JP.UTF-8
 "%CMDER_ROOT%\vendor\init.bat"
