ee913904411e44175702aefc9332c1fe9cd9180b
tsutsu <tsutsu@example.com>
2020-10-01 23:41:06 +0900

    wgetのインストールを追加

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ if [ ! -e $HOME/.tmux/plugins/tpm ]; then
 fi
 
 # python
-sudo apt install -y libsqlite3-dev libreadline-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev libffi-dev
+sudo apt install -y libsqlite3-dev libreadline-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev libffi-dev wget
 git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
