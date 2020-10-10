d01f35abeebd71aa003896cdf7f0fc85c6e01344
tsutsu <tsutsu@example.com>
2020-10-10 14:39:46 +0900

    docker-composeのインストールを追加

--- install.sh
+++ install.sh
@@ -131,6 +131,9 @@ sudo apt install -y docker-ce
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo systemctl restart docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # .NET Core
 cd /tmp/
