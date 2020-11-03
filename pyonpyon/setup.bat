eb52ae05fd2aceb6c550f9bd06c435f4c3fb3a13
tsutsu <tsutsu0928@gmail.com>
2020-11-03 23:17:31 +0900

    slのインストール先を変更

--- pyonpyon/setup.bat
+++ pyonpyon/setup.bat
@@ -3,23 +3,23 @@
 set WORK=%~dp0
 cd /d %WORK%
 
-REM sl clone & patch
+@REM sl clone & patch
 git clone https://github.com/sgymtic/sl.git
 cd ./sl
 git checkout pyonpyon
 patch -p1 < ../diff.patch
 
-REM PDCurses clone & build
+@REM PDCurses clone & build
 git clone https://github.com/wmcbrine/PDCurses.git
 cd PDCurses/wincon
 make -j2
 mv ./pdcurses.a ./libpdcurses.a
 
-REM sl build
+@REM sl build
 cd ../../
 make -j2
 
-REM make link
-mklink %SYSTEMROOT%\sl.exe %WORK%\sl\pyonpyon.exe
+@REM make link
+mklink %GOPATH%\bin\sl.exe %WORK%\sl\pyonpyon.exe
 
 cd /d %WORK%
