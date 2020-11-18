c501f839297ff0d803883d883edf345000b5cbe1
tsutsu <tsutsu0928@gmail.com>
2020-11-18 23:47:31 +0900

    peco用のbatを更新

--- windows/go/peco/peco-tool.bat
+++ windows/go/peco/peco-tool.bat
@@ -16,11 +16,8 @@ for /f "tokens=*" %%x in ('ghq list -p ^| peco') do (
 goto end
 
 :history
-for /f "tokens=*" %%x in ('cat -n %CMDER_ROOT%/config/.history ^| peco') do (
-  for /f "tokens=*" %%y in ('echo %%x ^| sed -r "s/ *[0-9]+ *(.+)$/\1/"') do (
-    %%y
-    break
-  )
+for /f "tokens=*" %%x in ('tac %CMDER_ROOT%/config/.history ^| peco') do (
+  %%x
   break
 )
 goto end
