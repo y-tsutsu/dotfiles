71204136e64870217e8bf177ddcdcaad4bab88af
tsutsu <tsutsu0928@gmail.com>
2022-08-14 12:10:24 +0900

    msys2のインストールスクリプトを更新

--- windows/msys2/install.bat
+++ windows/msys2/install.bat
@@ -4,5 +4,5 @@ set WORK=%~dp0
 cd /d %WORK%
 
 pacman -Syuu
-pacman -S base-devel msys2-devel mingw-w64-x86_64-toolchain
+pacman -S base-devel msys2-devel mingw-w64-x86_64-toolchain mingw64/mingw-w64-x86_64-clang
 pacman -Syuu
