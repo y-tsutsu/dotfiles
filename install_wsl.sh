2647048342c51cc98659da8822159caf109f96f2
tsutsu <tsutsu0928@gmail.com>
2023-08-05 15:18:13 +0900

    Debian12でのdotnetのインストールに対応

--- install_wsl.sh
+++ install_wsl.sh
@@ -111,7 +111,7 @@ curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 # .NET Core
 cd /tmp/
-wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
 sudo apt install -y apt-transport-https
