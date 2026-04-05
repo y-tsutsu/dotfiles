fadaaddfa5fae8fb8dd552462dd2b96c5a530cbd
tsutsu <tsutsu0928@gmail.com>
2026-04-05 21:45:31 +0900

    yaziでプレビューが動作していなかったのを修正

--- install.sh
+++ install.sh
@@ -158,7 +158,7 @@ git clone https://github.com/sxyazi/yazi.git
 cd yazi
 cargo build --release --locked
 sudo mv target/release/yazi target/release/ya /usr/local/bin/.
-sudo apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick
+sudo apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick file
 sudo apt install -y bat
 # move to setup.sh
 # ya pkg add dedukun/bookmarks
