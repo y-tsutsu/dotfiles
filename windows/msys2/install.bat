ace28ea867f9dd8e6e49ea8fc55c78ff47a33122
tsutsu <tsutsu0928@gmail.com>
2026-06-02 01:02:57 +0900

    msys2のインストールパッケージを更新

--- windows/msys2/install.bat
+++ windows/msys2/install.bat
@@ -4,5 +4,5 @@ set WORK=%~dp0
 cd /d %WORK%
 
 pacman -Syuu
-pacman -S base-devel msys2-devel mingw-w64-x86_64-toolchain mingw64/mingw-w64-x86_64-clang
+pacman -S base-devel msys2-devel mingw-w64-ucrt-x86_64-toolchain mingw-w64-ucrt-x86_64-clang mingw-w64-ucrt-x86_64-ninja
 pacman -Syuu
